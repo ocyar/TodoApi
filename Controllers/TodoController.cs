5424aeda31aa42c053862836558a2c9e7c036dbe
Rayco Hernandez <Rayco.Hernandez@tecalliance.net>
2019-08-18 19:53:48 +0100

    Segundo commit

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -21,6 +21,7 @@ namespace TodoApi.Controllers
             {
                 // Create a new TodoItem if collection is empty,
                 // which means you can't delete all TodoItems.
+                // Prueba segundo commit
                 _context.TodoItems.Add(new TodoItem { Name = "Item1" });
                 _context.SaveChanges();
             }
