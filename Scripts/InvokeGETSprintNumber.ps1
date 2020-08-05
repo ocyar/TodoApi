943817a78bb70598fc7d90c807aafa2c257859bc
ocyar <54065604+ocyar@users.noreply.github.com>
2020-08-05 22:41:33 +0100

    Add files via upload

--- Scripts/InvokeGETSprintNumber.ps1
+++ Scripts/InvokeGETSprintNumber.ps1
@@ -3,8 +3,10 @@ $token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes
 $header = @{authorization = "Basic $token"}
 $url = "https://tecalliance.visualstudio.com/TecOrder/TecConnect%205.0/_apis/work/teamsettings"
 $output = Invoke-RestMethod -Uri $url -Method Get -ContentType "application/json" -Headers $header
-Write-OutPut $output
 $defaultIterationName = $output.defaultIteration.name
+Write-OutPut "Default Iteration Name: $defaultIterationName"
 $sprintNumber = $defaultIterationName.substring(7)
-Write-OutPut "defaultIterationName: $defaultIterationName"
-Write-OutPut "sprintNumber: $sprintNumber"
+Write-OutPut "Sprint Number: $sprintNumber"
+Write-OutPut "BuildVersion before: $env:BuildVersion"
+$env:BuildVersion = $sprintNumber
+Write-OutPut "BuildVersion after: $env:BuildVersion"
